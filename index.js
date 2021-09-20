4df3031c4ab3b9cbd1354da6e2315d265543166d
Mark Momot <markmomot03@gmail.com>
2021-09-20 13:28:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion inside";
